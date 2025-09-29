01b7e475ab18216b4089b5f74b0338d740a461b9
jimebbelaar <jim.ebbelaar@gmail.com>
2025-09-29 09:56:11 +0200

    hero fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import { CheckCircle } from "lucide-react";
 
 export default function Hero() {
   return (
-    <section className="min-h-screen flex items-center justify-center pt-32 lg:pt-20 pb-10 relative overflow-hidden">
+    <section className="min-h-screen flex items-center justify-center pt-14 pb-10 relative overflow-hidden">
       {/* Background Effects */}
       <div className="absolute inset-0">
         <div className="absolute top-20 left-20 w-96 h-96 bg-adhd-yellow/20 rounded-full blur-3xl" />
