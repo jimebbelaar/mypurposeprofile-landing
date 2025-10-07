fd0a403258d57a8db70fe62522a1c27a2f2ae547
jimebbelaar <jim.ebbelaar@gmail.com>
2025-10-07 11:54:10 +0200

    4xl

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -40,7 +40,7 @@ export default function Hero() {
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: 0.1 }}
-                className="text-5xl sm:text-5xl lg:text-6xl mb-4"
+                className="text-4xl sm:text-4xl lg:text-6xl mb-4"
               >
                 Stop overthinking and 
                 <span className="gradient-text"> finally start your business </span>
